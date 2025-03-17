d54b1a02c2826512ef25e7f2526c585c6483fef9
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-03-18 01:31:10 +0530

    Corrected Url

--- Django_level_two/parTwo/appTwo/urls.py
+++ Django_level_two/parTwo/appTwo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from appTwo import views
 
 urlpatterns = [
-    path('', views.users,name='users'),
+    path('', views.index,name='index'),
+    path('users/', views.users,name='users'),
 ]
