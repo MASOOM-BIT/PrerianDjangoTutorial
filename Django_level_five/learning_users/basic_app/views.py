2d3f6ea53d067b6eeda3d00b3185fcd465b2f096
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-03-24 03:08:51 +0530

    Django Level 5 Project

--- Django_level_five/learning_users/basic_app/views.py
+++ Django_level_five/learning_users/basic_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+def base(request):
+    return render(request, 'basic_app/base.html')
+
+
+
+def registration(request):
+	return render(request,"basic_app/registration.html")
+
+def login(request):
+	return render(request,"basic_app/login.html")
