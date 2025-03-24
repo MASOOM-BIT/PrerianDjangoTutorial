a92a612107f75af1582d06bfa2d3816684ff7012
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-03-25 02:21:54 +0530

    Django Level 5 Project

--- Django_level_five/learning_users/basic_app/views.py
+++ Django_level_five/learning_users/basic_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm,UserProfileInfoForm
 
 # Create your views here.
 
@@ -12,7 +13,37 @@ def base(request):
 
 
 def registration(request):
-	return render(request,"basic_app/registration.html")
+	registered=False
+
+	if request.method=='POST':
+		user_form=UserForm(data=request.POST)
+		profile_form=UserProfileInfoForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+
+			user=user_form.save()
+			user.set_password(user.password)
+			user.save()
+
+			profile=profile_form.save(commit=False)
+
+			profile.user=user
+
+			if 'profile_pic' in request.FILES:
+				profile.profile_pic=request.FILES['profile_pic']
+
+			profile.save()
+
+			registered=True
+
+		else:
+			print(user_form.errors,profile_form.errors)
+
+	else:
+		user_form=UserForm()
+		profile_form=UserProfileInfoForm()
+
+	return render(request,"basic_app/registration.html",{'user_form':user_form,'profile_form':profile_form,'registered':registered})
 
 def login(request):
 	return render(request,"basic_app/login.html")
