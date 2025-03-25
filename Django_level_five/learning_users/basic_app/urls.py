5a8560f78e9f6a27af1d3e7d3a2019ca355d2bf4
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-03-26 02:30:59 +0530

    Django Level 5 Project

--- Django_level_five/learning_users/basic_app/urls.py
+++ Django_level_five/learning_users/basic_app/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from basic_app import views
 app_name = 'basic_app'
 urlpatterns = [
-    path('', views.index, name='index'),
     path('registration/', views.registration, name='registration'),
-    path('login/', views.login, name='login'),
-    path('base/', views.base, name='base'),
-
+    path('user_login/', views.user_login, name='user_login')
 ]
